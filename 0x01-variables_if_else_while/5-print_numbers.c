4d3afc2f67c8f54df70870818e00c7b887c1e2b5
Pelly <audreymphuthi78@gmail.com>
2023-02-17 19:14:58 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 /*
- * main - Prints all strings from 0 to 9
+ * main - A programme that prints all single digit numbers
+ * of base 10 starting from 0
+ *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int n;
+	int a;
 
-	for (n = 0; n < 10; n++)
-		printf("%d", n);
-	printf("\n");
+	for (a = 0; a <= 9; a++)
+	{
+		putchar(a + '0');
+	}
+	putchar('\n');
 	return (0);
 }
