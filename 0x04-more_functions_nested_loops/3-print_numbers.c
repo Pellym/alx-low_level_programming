b3636ed870290c1d6d1d6eef917a9203752c4347
Pelly <audreymphuthi78@gmail.com>
2023-02-23 11:50:22 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_numbers - print 0123456789
+* owned by Bwave ICT
+*Return: void
+*/
+
+void print_numbers(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+	_putchar(n);
+	}
+	_putchar('\n');
+}
