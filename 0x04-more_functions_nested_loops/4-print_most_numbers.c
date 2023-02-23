dd45e4c914b07ea9dfac0e6716ba0060add510c5
Pelly <audreymphuthi78@gmail.com>
2023-02-23 12:16:51 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*print_most_numbers - prints
+* owned by Bwave ICT
+*Return: void
+*/
+
+void print_most_numbers(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+	if (!(n == '2' || n == '4'))
+	_putchar(n);
+	}
+	_putchar('\n');
+}
