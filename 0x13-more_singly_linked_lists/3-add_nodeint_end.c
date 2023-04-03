5e8bc29e2881673bc00670bc20844a7341e1ccbf
Pellym <audreymphuthi78@gmial.com>
2023-04-03 16:47:19 -0700

    3-add_nodeint_end.c

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ *  frees a listint_t list
+ *
+ *  Return: Always 0
+ *
+ */
+
+void free_listint(listint_t *head)
+{
+	size_t counts = 0;
+		while (head != Null)
+		{
+			count++
+		        h = h->next;
+		}
+	return (count);
+}
