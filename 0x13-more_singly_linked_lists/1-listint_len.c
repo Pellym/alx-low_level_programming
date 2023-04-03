cc7a25ed6a3370c6b5b5229e41051a9c14fe480f
Pellym <audreymphuthi78@gmial.com>
2023-04-03 15:12:48 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,21 +1,19 @@
 #include "lists.h"
-#include <stddef.h>
 
-/*
- * prints all the elements of a listint_t list.
+/**
+ * listint_len - returns the number of elements in a linked listint_t list.
+ * @h: pointer to the linked list
  *
- * returns the number of elements in a linked listint_t list
+ * Return: number of elements in a linked listint_t list
  */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
 
- size_t listint_len(const listint_t *h)
- {
-                  size_t = 0;
-
-		  while (h != NULL)
-    {
-        count++;
-        h = h->next;
-    }
-
-    return count;
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
